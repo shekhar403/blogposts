5790d42f11ff8811add55e1dec8ed5920cd351cb
M Shekhar Dora <63382577+Shekhar403@users.noreply.github.com>
2023-05-10 15:47:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const heading = "Blogs - Web Development";
-const blogHeadings = ["Difference between HTTP1.1 vs HTTP2", "JavaScript Object"];
-const blogURLs = ["https://shekhar403.github.io/home/blogs/blog1/", "https://shekhar403.github.io/home/blogs/blog2/"];
+const blogHeadings = ["Difference between HTTP1.1 vs HTTP2", "JavaScript Object", "JavaScript Basics", "Permutations of a Number"];
+const blogURLs = ["https://shekhar403.github.io/home/blogs/blog1/", "https://shekhar403.github.io/home/blogs/blog2/", "https://webdiary360.blogspot.com/2023/05/js.html", "https://webdiary360.blogspot.com/2023/05/jspermutations.html"];
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));

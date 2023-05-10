07b38c97e267e8682b0d14b56fc1e28c48e8524e
M Shekhar Dora <63382577+Shekhar403@users.noreply.github.com>
2023-05-10 16:09:47 +0530

    update for date

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const app = express();
 const heading = "Blogs - Web Development";
 const blogHeadings = ["Difference between HTTP1.1 vs HTTP2", "JavaScript Object", "JavaScript Basics", "Permutations of a Number"];
 const blogURLs = ["https://shekhar403.github.io/home/blogs/blog1/", "https://shekhar403.github.io/home/blogs/blog2/", "https://webdiary360.blogspot.com/2023/05/js.html", "https://webdiary360.blogspot.com/2023/05/jspermutations.html"];
+const blogDates = ["16th April 2023", "16th April 2023", "10th May 2023", "10th May 2023"];
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 
 app.get("/", function(req, res) {
-    res.render("home", {Title : heading, Blogs: blogHeadings, URLs: blogURLs});
+    res.render("home", {Title : heading, Blogs: blogHeadings, URLs: blogURLs, Dates: blogDates});
 });
 
 app.listen(3000, function() {

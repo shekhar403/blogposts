2409e6982587fce1388996b9602f4783c86224ac
M Shekhar Dora <63382577+Shekhar403@users.noreply.github.com>
2023-05-26 20:59:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const heading = "Blogs - Web Development";
 const blogHeadings = ["Difference between HTTP1.1 vs HTTP2", "JavaScript Object", "JavaScript Basics", "Permutations of a Number"];
-const blogURLs = ["https://shekhar403.github.io/home/blogs/blog1/", "https://shekhar403.github.io/home/blogs/blog2/", "https://webdiary360.blogspot.com/2023/05/js.html", "https://webdiary360.blogspot.com/2023/05/jspermutations.html"];
-const blogDates = ["16th April 2023", "16th April 2023", "10th May 2023", "10th May 2023"];
+const blogURLs = ["https://shekhar403.github.io/home/blogs/blog1/", "https://shekhar403.github.io/home/blogs/blog2/", "https://webdiary360.blogspot.com/2023/05/js.html", "https://webdiary360.blogspot.com/2023/05/jspermutations.html", "https://shekhar403.github.io/home/blogs/blog3/"];
+const blogDates = ["16th April 2023", "16th April 2023", "10th May 2023", "10th May 2023", "26th May 2023"];
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));

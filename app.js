ac5201f516753b573837a9b92359b2a66082941a
M Shekhar Dora <63382577+Shekhar403@users.noreply.github.com>
2023-05-26 21:00:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const heading = "Blogs - Web Development";
-const blogHeadings = ["Difference between HTTP1.1 vs HTTP2", "JavaScript Object", "JavaScript Basics", "Permutations of a Number"];
+const blogHeadings = ["Difference between HTTP1.1 vs HTTP2", "JavaScript Object", "JavaScript Basics", "Permutations of a Number", "Understanding the Difference Between Document and Window Objects"];
 const blogURLs = ["https://shekhar403.github.io/home/blogs/blog1/", "https://shekhar403.github.io/home/blogs/blog2/", "https://webdiary360.blogspot.com/2023/05/js.html", "https://webdiary360.blogspot.com/2023/05/jspermutations.html", "https://shekhar403.github.io/home/blogs/blog3/"];
 const blogDates = ["16th April 2023", "16th April 2023", "10th May 2023", "10th May 2023", "26th May 2023"];
 
